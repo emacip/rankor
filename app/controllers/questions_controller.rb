da3701ff223cf744e3f73139689801f0dd0af6cb
emacip <emacipbelmonte@gmail.com>
2018-05-15 14:23:33 +0100

    Create a new question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,20 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def create
+    @question = Question.new(allowed_params)
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to  root_path, notice: 'Question successfully updated.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  def allowed_params
+    params.require(:question).permit(:title, :body, :created_at, :updated_at )
+  end
+
 end
--- spec/features/questions_management_spec.rb
+++ spec/features/questions_management_spec.rb
@@ -15,7 +15,8 @@ RSpec.feature "Questions management"  do
   scenario "User creates a new question" do
     given_an_user_logged_with_google_account
     click_link "New Question"
-    expect(page).to have_content("New Question")
+    when_i_create_a_question
+    i_should_see_question_in_my_page
   end
 
   def given_an_user_logged_with_google_account
@@ -27,7 +28,23 @@ RSpec.feature "Questions management"  do
     expect(page).to have_content("Question_1")
   end
 
+  def when_i_create_a_question
+    expect(page).to have_content("New Question")
+    fill_in 'question[title]', with: 'Who directed the Star Wars movies?'
+    fill_in 'question[body]', with: '** George Lucas **'
+    submit_form
+  end
+
+  def i_should_see_question_in_my_page
+    expect(page).to have_current_path root_path
+    expect(page).to have_content('Who directed the Star Wars movies?')
+  end
+
   def load_questions
     FactoryBot.create(:question)
+  end
+
+  def submit_form
+    find('input[name="commit"]').click
   end
 end
